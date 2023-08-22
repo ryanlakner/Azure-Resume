f03a169b7c51889e9ac1d68e8002960d56e5cec8
ryanlakner <ryanlakner@live.com>
2023-08-22 11:49:09 -0400

    Added structure

---
+++ backend/api/Counter.cs
@@ -0,0 +1,12 @@
+using Newtonsoft.json;
+
+namespace Company.Function
+{
+    public class Counter
+    {
+        [JsonProperty(PropertyName="id")]
+        public string Id {get; set;}
+        [JsonProperty(PropertyName="count")]
+        public int Count {get; set;}
+    }
+}
